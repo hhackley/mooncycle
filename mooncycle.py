9bd27574208275b3b24fd9ad973bdbaf0303f0d1
HunterHackley <90525453+Kypressss@users.noreply.github.com>
2023-05-21 12:58:17 -0500

    Added search for moon cycle based on address

--- mooncycle.py
+++ mooncycle.py
@@ -12,16 +12,46 @@ def get_coordinates(address):
     else:
         return None
 
+def get_moon_phase(latitude, longitude):
+    observer = ephem.Observer()
+    observer.lat = str(latitude)
+    observer.lon = str(longitude)
+
+    moon = ephem.Moon()
+    moon.compute(observer)
+
+    phase = moon.phase / 100  # Convert phase value to fraction
+
+    if 0 <= phase < 0.03 or phase >= 0.97:
+        moon_phase = 'New Moon'
+    elif 0.03 <= phase < 0.5:
+        moon_phase = 'First Quarter Waxing Crescent'
+    elif 0.5 <= phase < 0.53:
+        moon_phase = 'First Quarter Half Moon'
+    elif 0.53 <= phase < 0.97:
+        moon_phase = 'First Quarter Waxing Gibbous'
+    else:
+        moon_phase = 'Full Moon'
+
+    return moon_phase
+
+
 # Prompt the user to enter an address
-print("---- Getting Latitude and Longitude ----")
+print("\n---- Getting Latitude and Longitude ----")
 address = input("Enter an address: ")
 
 # Retrieve the latitude and longitude for the address
 coordinates = get_coordinates(address)
 if coordinates:
     latitude, longitude = coordinates
+    print("\n---- Coordinates ----")
     print("Latitude:", latitude)
     print("Longitude:", longitude)
+
+    # Get the moon phase for the provided coordinates
+    moon_phase = get_moon_phase(latitude, longitude)
+    print("\n---- Moon Phase ----")
+    print("Moon Phase:", moon_phase)
+    print("\n")
 else:
     print("Unable to retrieve coordinates for the address.")
-    
